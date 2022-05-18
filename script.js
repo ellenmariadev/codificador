562a50d99e26a47261d13e445389dbe845167219
ellenmariadev <ellenmariadev@gmail.com>
2022-05-18 14:09:54 -0300

    codificação cifraCesar

--- script.js
+++ script.js
@@ -14,6 +14,51 @@ selecione.addEventListener("click", function () {
   }
 });
 
+// Base64
+
+function base64() {
+  let mensagem = document.querySelector("#mensagem").value;
+
+  if (codificar.checked) {
+    let codificado = btoa(mensagem);
+    return codificado;
+  } else if (decodificar.checked) {
+    let decodificado = atob(mensagem);
+    return decodificado;
+  }
+}
+
+// Cifra de César
+
+function cifraCesar() {
+  let msg = document.querySelector("#mensagem").value;
+  let chave = parseInt(document.querySelector("#rangenumber").value);
+  let saida = '';
+
+  if (codificar.checked) {
+    for (let i = 0; i < msg.length; i++) {
+      if (msg[i] === msg[i].toUpperCase()) {
+        saida += String.fromCharCode((msg.charCodeAt(i) + chave - 65) % 26 + 65); 
+      } else {
+        saida += String.fromCharCode((msg.charCodeAt(i) + chave - 97) % 26 + 97);
+      }
+    }
+    return saida;
+
+  } else if (decodificar.checked) {
+    for (let i = 0; i < msg.length; i++) {
+      if (msg.charCodeAt(i) >= 97 && msg.charCodeAt(i) <= 122) {
+        saida += String.fromCharCode((msg.charCodeAt(i) - 97 -  chave + 26) % 26 + 97);
+      } else if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
+        saida += String.fromCharCode((msg.charCodeAt(i) - 65 - chave + 26) % 26 + 65);
+      } else {
+        saida += String.fromCharCode(msg.charCodeAt(i));
+      }
+    }
+    return saida;
+  }
+}
+
 // Botão
 radiobtn.addEventListener("click", function () {
   if (codificar.checked) {
@@ -23,25 +68,11 @@ radiobtn.addEventListener("click", function () {
   }
 });
 
-// Base64
-
-function base64() {
-  let mensagem = document.querySelector("#mensagem").value;
-
-  if (codificar.checked === true) {
-    let codificado = btoa(mensagem);
-    return codificado;
-  } else if (decodificar.checked === true) {
-    let decodificado = atob(mensagem);
-    return decodificado;
-  }
-}
-
 btn.addEventListener("click", function (event) {
   event.preventDefault();
   if (selecione.value === "base64") {
     resultado.innerText = base64();
-  } else {
-    return 0;
+  } else if (selecione.value === "cifra") {
+    resultado.innerText = cifraCesar();
   }
 });
